a47a92ef8b1c420444f53a248cc8a5abd873879d
Annie Daniel <adaniel@texastribune.org>
2014-12-16 17:35:00 -0600

    added num to json for slide slugs and the ugliest view all button in the world

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,21 @@
 'use strict';
 
+$(document).ready(function() {
+
+  $('.slide-0').addClass('active');
+
+  $('.slide-btn.next').on('click', function(){
+    var activeSlide = $('.active'),
+      idx = activeSlide.index(),
+      nextSlide = idx += 1;
+      $(activeSlide).removeClass('active');
+      $('.slide').index(nextSlide).addClass('active');
+      console.log(idx);
+      console.log(nextSlide);
+  });
+
+  $('.view-all').on('click', function(){
+    $('.slide').addClass('active');
+  });
+
+});
