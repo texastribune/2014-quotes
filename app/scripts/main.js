3f9999b9e36a6a065bdc512021b427f7c0308081
Annie Daniel <adaniel@texastribune.org>
2014-12-17 14:39:50 -0600

    reset at the end of the slideshow

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,32 +2,32 @@
 
 $(document).ready(function() {
 
-  // function showQuoteSlide(index){
-  //    $('.slide.slide-' + slug).addClass('active').siblings('.active').removeClass('active');
-  // };
-
-
+  //first quote appears
   $('.slide').eq(0).addClass('active');
 
+  //prev/next buttons being buttons
   $('.slide-btn').on('click', function(){
     var activeSlide = $('.slide.active'),
       idx = $('.slide').index(activeSlide),
       nextSlide = $(this).hasClass('next') ? idx + 1 : idx - 1;
     $('.slide').eq(nextSlide).addClass('active').siblings('.active').removeClass('active');
     console.log(idx);
-    if (idx === 12){
-      idx === 0;
-    };
-  });
 
-  $(document).keydown(function(e) {
-    if (e.which == 37) { // left key
-        $('.prev').trigger('click');
-    } else if (e.which == 39) { // right key
-        $('.next').trigger('click');
+    //reset at the end
+    if (idx === 12){
+      $('.slide').eq(0).addClass('active').siblings('.active').removeClass('active');
     }
   });
 
+  // oh cool, it works with keyboard arrows too!
+  $(document).keydown(function(e) {
+    if (e.which === 37) { // left key
+        $('.prev').trigger('click');
+    } else if (e.which === 39) { // right key
+        $('.next').trigger('click');
+    }
+  });
+  // all of the quotes
   $('.view-all').on('click', function(){
     $('.slide').addClass('active');
     $('.slide-btn').addClass('hidden');
